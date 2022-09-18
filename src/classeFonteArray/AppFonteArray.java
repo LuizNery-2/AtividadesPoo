f12f92febf55c7fafc96fe369ea25365e5557f5e
LuizNery-2 <62023495+LuizNery-2@users.noreply.github.com>
2022-09-18 15:02:34 -0300

    Exercício Classe Fonte Array

---
+++ src/classeFonteArray/AppFonteArray.java
@@ -0,0 +1,82 @@
+package classeFonteArray;
+
+import java.util.Scanner;
+
+public class AppFonteArray {
+
+	public static void main(String[] args) {
+		
+		Fonte[] fontes = new Fonte[3];
+		Scanner input = new Scanner(System.in);
+		
+		int codigo;
+		String descricao;
+		double precoCusto;
+		
+		
+		for (int i = 0; i < fontes.length; i++) {
+			
+			while (true) {
+			
+				System.out.printf("\nDigite o codigo do produto da %d fonte: ", i+1);
+				
+				codigo = input.nextInt();
+				
+				
+				if(codigo <= 0) {
+					
+					System.out.println("digite um codigo maior que 0");
+					
+					continue;
+					
+					
+				}
+				else {
+					input.nextLine();
+					break;
+				}
+				
+			}
+			
+			
+			System.out.printf("\nDescreva o produto da %d fonte: ", i+1);
+			
+			descricao = input.next();
+			
+			while(true) 
+			{
+				
+				System.out.printf("\nInforme o preço do produto %d fonte: ", i+1);
+				
+				precoCusto = input.nextDouble();
+				
+				if(precoCusto <= 0) {
+					
+					System.out.println("O preço deve ser maior que 0");
+					
+					continue;
+					
+					
+				}
+				else {
+					
+					break;
+				}
+				
+				
+			}
+			
+			fontes[i] = new Fonte(codigo, descricao, precoCusto); 
+				
+				
+			
+		}
+
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/classeFonteArray/Fonte.java
@@ -0,0 +1,73 @@
+package classeFonteArray;
+
+public class Fonte {
+	
+	private int codigo;
+	private String descricao;
+	private int estoque;
+	private double precoCusto;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public int getEstoque() {
+		return estoque;
+	}
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+	public double getPrecoCusto() {
+		return precoCusto;
+	}
+	public void setPrecoCusto(double precoCusto) {
+		this.precoCusto = precoCusto;
+	}
+	
+	public Fonte(int codigo, String descricao,Double precoCusto ) {
+		
+		this.codigo = codigo;
+		this.descricao = descricao;
+		this.precoCusto = precoCusto;
+		this.estoque = 0;
+		
+		
+	}
+	
+	public void registrarProducao(int quantProduzida) {
+		
+		estoque += quantProduzida;
+		
+	}
+	
+	public void resgitrarEscoamento(int quantEscoada) {
+		
+		if (quantEscoada >= estoque) {
+			estoque -= 0;
+		}
+		else {
+			estoque -= quantEscoada;
+		}
+		
+		
+	}
+	
+	public double obterValorEstoque() {
+		
+		return precoCusto * estoque;
+	}
+	
+	
+	
+	
+	
+	
+}
